95d49d8864d56dd49d1383c88f6999238b9e230f
Chris Eady <chris.eady1@gmail.com>
2016-01-24 11:20:21 -0600

    removed debug p statements in bubble_sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,9 +1,6 @@
 def bubble_sort(arr)
   (arr.length-1).times do
     (arr.length-1).times do |i|
-      p arr
-      p arr[i].to_s + ', ' + arr[i+1].to_s
-
       if arr[i] > arr[i+1]
         arr[i], arr[i+1] = arr[i+1], arr[i]
       end
@@ -12,6 +9,6 @@ def bubble_sort(arr)
   return arr
 end
 
-sorted_array = bubble_sort([3,2,1])
+sorted_array = bubble_sort([3,5,3,7,5,7,3,88,33,45,2,1])
 
 p sorted_array
