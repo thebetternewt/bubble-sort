523c9347680424ab8fba2e52b599d439b52f31dc
Chris Eady <chris.eady1@gmail.com>
2016-01-24 11:57:00 -0600

    second method added that accepts sorting method block

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -9,6 +9,19 @@ def bubble_sort(arr)
   return arr
 end
 
-sorted_array = bubble_sort([3,5,3,7,5,7,3,88,33,45,2,1])
+def bubble_sort_by(arr)
+  (arr.length-1).times do
+    (arr.length-1).times do |i|
+      if yield(arr[i],arr[i+1]) == 1
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+      end
+    end
+  end
+  return arr
+end
 
-p sorted_array
+
+# sorted_array = bubble_sort([3,5,3,7,5,7,3,88,33,45,2,1])
+# p sorted_array
+
+p bubble_sort_by(['zebras', 'giraffes', 'elephants']) { |val1, val2| val1 <=> val2 }
