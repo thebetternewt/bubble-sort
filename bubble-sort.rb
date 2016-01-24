fde85d45fcb0ae128c0c65133193d59bd00f49a0
Chris Eady <chris.eady1@gmail.com>
2016-01-24 11:12:26 -0600

    first commit - single iteration through array

---
+++ bubble-sort.rb
@@ -0,0 +1,16 @@
+def bubble_sort(arr)
+    (arr.length-1).times do |i|
+      p arr
+      p arr[i].to_s + ', ' + arr[i+1].to_s
+
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+      end
+
+    end
+  return arr
+end
+
+sorted_array = bubble_sort([3,2,1])
+
+p sorted_array
