9a38ca233d0e04aaee2c3fd681cd0656449aa17d
Chris Eady <chris.eady1@gmail.com>
2016-01-24 11:17:19 -0600

    iterates through array number of times equal to number of items in array

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,4 +1,5 @@
 def bubble_sort(arr)
+  (arr.length-1).times do
     (arr.length-1).times do |i|
       p arr
       p arr[i].to_s + ', ' + arr[i+1].to_s
@@ -6,8 +7,8 @@ def bubble_sort(arr)
       if arr[i] > arr[i+1]
         arr[i], arr[i+1] = arr[i+1], arr[i]
       end
-
     end
+  end
   return arr
 end
 
